a8a17ad73be325697b006b23e876347281ab825a
Junnior Sauceda <junnior.sauceda05@unitec.edu.hn>
2023-08-18 14:41:49 -0600

    Creacion de la clase Equipo

--- src/paquete/Equipo.java
+++ src/paquete/Equipo.java
@@ -4,10 +4,73 @@
  */
 package paquete;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Junnior Sauceda
  */
 public class Equipo {
+    private String Nombre, pais;
+    private int rating;
+    private ArrayList <Jugador> plantilla=new ArrayList<>();
+
+    public Equipo() {
+    }
+
+    
+    public Equipo(String Nombre, String pais) {
+        this.Nombre = Nombre;
+        this.pais = pais;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        int acum=0;
+        for (Jugador jugador : plantilla) {
+            acum+=jugador.getRating();
+        }
+        this.rating=acum/plantilla.size();
+    }
+
+    public ArrayList<Jugador> getPlantilla() {
+        return plantilla;
+    }
+
+    public void setPlantilla(ArrayList<Jugador> plantilla) {
+        if(plantilla.size()>4){
+            
+        }
+        else{
+            this.plantilla = plantilla;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return Nombre + " - " + pais ;
+    }
+    
+    
+    
     
 }
